15d61476ebc11f41616d78133ec1f96370b04fc9
Omkar Tavva <143606533+Omkar-t06@users.noreply.github.com>
2024-07-14 06:08:14 +0000

    day 2 tasks

---
+++ day_2.js
@@ -0,0 +1,55 @@
+// Activity 1: Arithmetic Operations
+let a = 10 ;
+let b = 20; 
+
+// Task 1: Write a program to add 2 no. and log the value to the console
+let res = a + b;
+console.log(res);
+
+// Task 2: Write a program to substract 2 no. and log the value to the console
+res = a - b;
+console.log(res);
+
+// Task 3: Write a program to multiply 2 no. and log the value to the console
+res = a * b;
+console.log(res);
+
+// Task 4: Write a program to divide 2 no. and log the value to the console
+res = a / b;
+console.log(res);
+
+// Task 5: Write a program to find the remainder when one no. is divided by another no. and log the value to the console
+res = b % a;
+console.log(res);
+
+// Activity 2: Assignment Operations
+// Task 6: Use the `+=` operator to a variable and log the value to console
+a += b;
+console.log(a);
+// Task 7: Use the `-=` operator to a variable and log the value to console
+a -= b;
+console.log(a);
+
+// Activity 3: Comparison Operations
+// Task 8: Write a program to compare two numbers using < and > log the result to the console.
+console.log( a < b);
+console.log( a > b);
+// Task 9: Write a program to compare two numbers using <= and => log the result to the console.
+console.log(6 <= 6);
+console.log(19 >= 6);
+// Task 10: Write a program to compare two numbers using == and === log the result to the console.
+let str1 = 'mani'
+let str2 = 'man'
+console.log(str1 == str2);
+console.log(str1 === str2);
+
+// Activity 4: Logical Operators
+
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+console.log(a > b && a < b);
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+console.log(a > b || a < b);
+
+// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+console.log(!(a > b));
