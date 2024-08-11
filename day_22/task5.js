794b7dbdf81889f7e8740059e26f08565b91bd35
Omkar Tavva <143606533+Omkar-t06@users.noreply.github.com>
2024-08-11 16:26:09 +0000

    Day 22 - Task 5

--- day_22/task5.js
+++ day_22/task5.js
@@ -2,3 +2,22 @@
 
 // Task 5: Solve the "Group Anagrams" problem on LeetCode.
 // Write a function that takes an array of strings and groups anagrams together. Log the grouped anagrams for a few test cases
+
+function groupAnagrams(strs) {
+    const map = new Map();
+
+    for(let str of strs) {
+        const sortedStr = str.split('').sort().join('')
+        if(!map.has(sortedStr)) {
+            map.set(sortedStr,[])
+        }
+        map.get(sortedStr).push(str)
+    }
+
+    return Array.from(map.values());
+}
+
+// Test cases
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [["eat","tea","ate"],["tan","nat"],["bat"]]
+console.log(groupAnagrams([""])); // [[""]]
+console.log(groupAnagrams(["a"])); // [["a"]]
