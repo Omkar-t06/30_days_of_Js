f3cfabb664071de9d148a66160b171b9d63498b0
Omkar Tavva <143606533+Omkar-t06@users.noreply.github.com>
2024-08-16 17:54:52 +0000

    Day 25 Basic tasks

---
+++ day_25/index.js
@@ -0,0 +1,63 @@
+const apiKey = 'a78eb45c';
+// const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=Inception`;
+
+// fetch(apiUrl)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.error('Error fetching data:', err))
+
+document.getElementById("search-btn").addEventListener('click',() => {
+    const query = document.getElementById('search-input').value;
+    const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`;
+    // console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(
+            data => {
+                const results = document.getElementById('movie-results');
+                // console.log(results);
+                
+                results.innerHTML = ''
+
+                if(data.Search) {
+                    data.Search.forEach(movie => {
+                        const movieElement = document.createElement('div');
+                        movieElement.classList.add('movie')
+                        movieElement.innerHTML = `
+                            <img src="${movie.Poster}" alt="${movie.Title}">
+                            <div>
+                                <h2>${movie.Title}</h2>
+                                <p>Year: ${movie.Year}</p>
+                                <p>Type: ${movie.Type}</p>
+                                <button onclick="fetchMovieDetails('${movie.imdbID}')">More Info</button>
+                            </div>
+                        `;
+                        results.appendChild(movieElement);
+                    });
+                } else {
+                    results.innerHTML = '<p>No Results Found</p>'
+                }
+            }
+        ).catch (
+            err => console.error('Error fetching data:', err)
+        )
+});
+
+function fetchMovieDetails(imdbID) {
+    const detailsUrl = `https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}&plot=full`;
+
+    fetch(detailsUrl)
+        .then(response => response.json())
+        .then(data => {
+            const movieElement = document.createElement('div');
+            movieElement.innerHTML = `
+                <h3>${data.Title}</h3>
+                <p>Plot: ${data.Plot}</p>
+                <p>Director: ${data.Director}</p>
+                <p>Actors: ${data.Actors}</p>
+            `;
+            document.getElementById('movie-results').appendChild(movieElement);
+        })
+        .catch(error => console.error('Error fetching details:', error));
+}
